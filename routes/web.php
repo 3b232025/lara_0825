abe21eaedadace67b32f985bef91c47f283f261c
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:06:47 +0800

    用save新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $post = new Post();            
+    $post->title = 'test title';   
+    $post->content = 'test content';
+    $post->save();                 
+    return $post;                  
 });
