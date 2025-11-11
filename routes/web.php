1563a53b4d0c56aee26f92f13c07440ceedcc8ff
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:28:15 +0800

    使用where方法查詢資料

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,11 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $posts = Post::all();
-
+    $posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
+    
     foreach ($posts as $post) {
-        echo "ID: {$post->id}<br>";
-        echo "標題: {$post->title}<br>";
-        echo "內容: {$post->content}<br>";
-        echo "建立時間: {$post->created_at}<br>";
-        echo "----------------------<br>";
+        echo "ID: {$post->id} | 標題: {$post->title}<br>";
     }
-    dd($post);
+
+    dd($posts);
 });
