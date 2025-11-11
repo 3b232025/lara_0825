9b57ec7d151ca5844e7ea1813ea831df90f8fecc
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:32:12 +0800

    使用update方法更新資料

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $posts = Post::where('id','<','10')->orderBy('id','DESC')->get();
-    
-    foreach ($posts as $post) {
-        echo "ID: {$post->id} | 標題: {$post->title}<br>";
-    }
-
-    dd($posts);
+    $post = Post::find(1);          
+    $post->update([
+        'title'   => 'updated title',
+        'content' => 'updated content',
+    ]);        
 });
