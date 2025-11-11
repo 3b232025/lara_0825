432550b7bfd4a179833b1c4d90dd1c1229876a62
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:22:28 +0800

    使用all方法查詢資料

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $post = Post::find(1);
+    $posts = Post::all();
 
-    echo "ID: {$post->id}<br>";
-    echo "標題: {$post->title}<br>";
-    echo "內容: {$post->content}<br>";
-    echo "建立時間: {$post->created_at}<br>";
-
+    foreach ($posts as $post) {
+        echo "ID: {$post->id}<br>";
+        echo "標題: {$post->title}<br>";
+        echo "內容: {$post->content}<br>";
+        echo "建立時間: {$post->created_at}<br>";
+        echo "----------------------<br>";
+    }
     dd($post);
 });
