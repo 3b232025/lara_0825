6614e5b8f22a2e728129219da4b46de3ce10d57c
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:33:59 +0800

    使用save方法更新資料

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $post = Post::find(1);          
-    $post->update([
-        'title'   => 'updated title',
-        'content' => 'updated content',
-    ]);        
+    $post = Post::find(1);
+    $post->title   = 'saved title';
+    $post->content = 'saved content';
+    $post->save();                   
 });
