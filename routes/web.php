1d4dfa46f4ce1d6dc7f1c57a06b2a4a1c56b761b
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-11 19:20:44 +0800

    使用find方法查詢資料

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $post = Post::create([
-        'title'      => 'test title',
-        'content'    => 'test content',
-        'is_feature' => true,
-    ]);
-    return $post;            
+    $post = Post::find(1);
+
+    echo "ID: {$post->id}<br>";
+    echo "標題: {$post->title}<br>";
+    echo "內容: {$post->content}<br>";
+    echo "建立時間: {$post->created_at}<br>";
+
+    dd($post);
 });
