71e4858591479f894eccbfd7654d8fd4db41823e
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-25 17:07:24 +0800

    使用 find 方法取得單一貼文（Model）

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $featuredPosts = \App\Models\Post::where('is_feature', 1)->get();
-dd($featuredPosts);              
+ $fourthPost = \App\Models\Post::find(4);
+dd($fourthPost);             
 });
