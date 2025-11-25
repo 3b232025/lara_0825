9cc1cdd2ed94e739c9a24bae39d0928a20b41ee6
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-25 17:06:44 +0800

    使用 where 方法取得 is_feature=1 的貼文集合

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-     $allPosts = Post::all();
-    dd($allPosts);                  
+    $featuredPosts = \App\Models\Post::where('is_feature', 1)->get();
+dd($featuredPosts);              
 });
