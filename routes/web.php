29b0ab92e065f35187149e28440317b91469c88e
3b232025 <3b232025@gm.student.ncut.edu.tw>
2025-11-25 17:05:45 +0800

    使用 all 方法取得多筆貼文的 Collection

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $post = Post::destroy(3, 5 ,7);                  
+     $allPosts = Post::all();
+    dd($allPosts);                  
 });
